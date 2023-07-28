02e7a92bfe6f04f0a8f0315fbe7e71847173a5e2
Soham Roy <sohamroy20052005@gmail.com>
2023-07-28 10:23:25 +0530

    Commit 1

---
+++ SRC/meow.py
@@ -0,0 +1,4 @@
+from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
